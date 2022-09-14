efe145dbf20a104498d334e7eabe3f3064b85220
Aditya Kumar <adityakumarsanni.2001@gmail.com>
2022-09-14 16:09:19 +0530

    octant_input file imported using pandas

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,8 +1,19 @@
-def factorial(x):
-    if(x<0):
-        return -1
-    if(x==0):
-        return 1
-    return x*factorial(x-1)
-x=int(input("Enter the number whose factorial is to be found: "))
-print(factorial(x))
+import pandas as pd
+def octant_identification(mod=5000):
+    #reading octant_input.csv file using pandas module
+    octant_ip=pd.read_csv("octant_input.csv")
+    print(octant_ip.head())
+
+    
+
+
+from platform import python_version
+ver = python_version()
+
+if ver == "3.8.10":
+    print("Correct Version Installed")
+else:
+    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+
+mod=5000
+octant_identification(mod)

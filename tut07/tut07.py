574c328d619bccccba253e89e1507c884426a5a0
Aditya Kumar <adityakumarsanni.2001@gmail.com>
2022-11-19 16:20:52 +0530

    function for setting octant sign for points created

--- tut07/tut07.py
+++ tut07/tut07.py
@@ -29,6 +29,149 @@ double = Side(border_style="thin", color=black)
 blackBorder = Border(top=double, left=double, right=double, bottom=double)
 
 ###Code
+
+# Method based on if-else to return octant type
+def get_octant(x,y,z):
+    if(x>=0 and y>=0):
+        if(z>=0):
+            return 1
+        else:
+            return -1
+    
+    if(x<0 and y>=0):
+        if(z>=0):
+            return 2
+        else:
+            return -2
+
+    if(x<0 and y<0):
+        if(z>=0):
+            return 3
+        else:
+            return -3
+
+    if(x>=0 and y<0):
+        if(z>=0):
+            return 4
+        else:
+            return -4
+
+def setProcessedDataWithOctant(u_avg, v_avg, w_avg, total_count, inputSheet, outputSheet):
+    start = 2
+    time = inputSheet.cell(start, 1).value
+
+    # Iterating through out the sheet
+    while(time!=None):
+        # Calculating processed data
+        try:
+            u1 = inputSheet.cell(start, 2).value - u_avg
+            v1 = inputSheet.cell(start, 3).value - v_avg
+            w1 = inputSheet.cell(start, 4).value - w_avg
+            
+            u1 = round(u1,3)
+            v1 = round(v1,3)
+            w1 = round(w1,3)
+
+            oct = get_octant(u1, v1, w1)
+        except FileNotFoundError:
+            print("Input file not found!!")
+            exit()
+        except ValueError:
+            print("Row or column values must be at least 1 ")
+            exit()
+
+        # Setting processed data
+        try:
+            outputSheet.cell(row=start+1, column=8).value = u1
+            outputSheet.cell(row=start+1, column=9).value = v1
+            outputSheet.cell(row=start+1, column=10).value = w1
+            outputSheet.cell(row=start+1, column=11).value = oct
+        except FileNotFoundError:
+            print("Output file not found!!")
+            exit()
+        except ValueError:
+            print("Row or column values must be at least 1 ")
+            exit()
+
+        start = start+1
+        try:
+            time = inputSheet.cell(start, 1).value
+        except FileNotFoundError:
+            print("Input file not found!!")
+            exit()
+        except ValueError:
+            print("Row or column values must be at least 1 ")
+            exit()
+
+def set_input_data(input_file_name, outputSheet):
+	input_file = openpyxl.load_workbook(input_file_name)
+	inputSheet = input_file.active
+
+	start = 2
+	time = inputSheet.cell(start, 1).value
+
+    # Variables to store sum variable
+	u_sum = 0 
+	v_sum = 0
+	w_sum = 0
+
+	# Iterating complete file till time value is not None
+	while(time!=None):
+		try:
+			u_sum += float(inputSheet.cell(start, 2).value)
+			v_sum += float(inputSheet.cell(start, 3).value)
+			w_sum += float(inputSheet.cell(start, 4).value)
+		except ValueError:
+			print("Sheet input can't be converted to float!!")
+			exit()
+		except TypeError:
+			print("Sheet doesn't contain valid float input!!")
+			exit()
+
+		try:
+			# Setting input time,u,v,w values
+			outputSheet.cell(row=start+1, column=1).value = inputSheet.cell(start, 1).value 
+			outputSheet.cell(row=start+1, column=2).value = inputSheet.cell(start, 2).value 
+			outputSheet.cell(row=start+1, column=3).value = inputSheet.cell(start, 3).value 
+			outputSheet.cell(row=start+1, column=4).value = inputSheet.cell(start, 4).value 
+		except FileNotFoundError:
+			print("File not found!!")
+			exit()
+		except ValueError:
+			print("Row or column values must be at least 1 ")
+			exit()
+
+		start = start+1
+		time = inputSheet.cell(start, 1).value
+
+	# Setting total count
+	total_count = start-2 # -1 for header and -1 for last None
+	# Calculating average
+	try:
+		u_avg = round(u_sum/total_count, 3)
+		v_avg = round(v_sum/total_count, 3)
+		w_avg = round(w_sum/total_count, 3)
+	except ZeroDivisionError:
+		print("No input data found!!\nDivision by zero occurred!")
+		exit()
+
+	# Setting average values
+	try:
+		outputSheet.cell(row=3, column=5).value = u_avg
+		outputSheet.cell(row=3, column=6).value = v_avg
+		outputSheet.cell(row=3, column=7).value = w_avg
+	except FileNotFoundError:
+		print("Output file not found!!")
+		exit()
+	except ValueError:
+		print("Row or column values must be at least 1 ")
+		exit()
+
+	# Processing input
+	setProcessedDataWithOctant(u_avg, v_avg, w_avg, total_count, inputSheet, outputSheet)
+
+	return total_count
+
 def entry_point(input_file, mod):
 	fileName = input_file.split("\\")[-1]
 	fileName = fileName.split(".xlsx")[0]
@@ -47,7 +190,8 @@ def entry_point(input_file, mod):
 	headers = ["T", "U", "V", "W", "U Avg", "V Avg", "W Avg", "U'=U - U avg", "V'=V - V avg","W'=W - W avg", "Octant"]
 	for i, header in enumerate(headers):
 		outputSheet.cell(row=2, column=i+1).value = header
-		
+
+	total_count = set_input_data(input_file, outputSheet)
 	outputFile.save(outputFileName)
 
 def octant_analysis(mod=5000):

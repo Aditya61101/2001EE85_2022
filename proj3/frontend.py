2f9ea238417c94eda7716f2c7fe96c409895d177
Aditya Kumar <adityakumarsanni.2001@gmail.com>
2022-11-29 23:17:17 +0530

    dropdown, loading spinner, project title added

--- proj3/frontend.py
+++ proj3/frontend.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import subprocess
+from datetime import datetime
 
 # sets the title of the page
 st.set_page_config(page_title='PSAT_V3')
@@ -14,6 +15,9 @@ if "filter" not in st.session_state:
 def take_input():
     try:
         # taking inputs for constant_fk2d, multiplying_factor, Shear_velocity
+        st.markdown("---")
+        st.markdown("<h1 style='text-align: center;'>Project PSAT v3.0</h1>", unsafe_allow_html=True)
+        st.markdown("---")
         constant_fk2d_value = st.number_input('Enter constant_fk2d_value', step=.01, format="%.2f")
 
         multiplying_factor_value = st.number_input('Enter multiplying_factor_value', step=.01, format="%.2f")
@@ -28,7 +32,8 @@ def take_input():
             st.session_state['filter'] = True
             if constant_fk2d_value>0 and multiplying_factor_value>0 and Shear_velocity_value>0:
                 st.text(" 1. C\n 2. S\n 3. A\n 4. C & S\n 5. C & A\n 6. S & A\n 7. C & S & A\n 8. All Combine")
-                option = st.number_input("Choose filtering method from above", min_value=0, max_value=8, step=1)
+                # option = st.number_input("Choose filtering method from above", min_value=0, max_value=8, step=1)
+                option = st.selectbox("Choose the filtering method from above", options=('Select here',1,2,3,4,5,6,7,8), key='option')
                 if option == 1:
                     corr = st.number_input('Enter Threshold value of C', step=1)
                 elif option == 2:
@@ -55,12 +60,21 @@ def take_input():
                 if st.button("Go to Replace") or st.session_state.replace:
                     if option>0:
                         st.session_state['replace'] = True
-                        st.text(" 1. Previous Point\n 2. 2*last-2nd_last\n 3. Overall_Mean\n 4. 12_Point_Strategy\n 5. Mean Of Previous 2 Points\n 6. All Sequential\n 7. All Parallel\n")
-                        replacement_method = st.number_input("Choose Replacement Method From Above ", step=1, min_value=1, max_value=7)
+                        # st.text(" 1. Previous Point\n 2. 2*last-2nd_last\n 3. Overall_Mean\n 4. 12_Point_Strategy\n 5. Mean Of Previous 2 Points\n 6. All Sequential\n 7. All Parallel\n")
+                        # replacement_method = st.number_input("Choose Replacement Method From Above ", step=1, min_value=1, max_value=7)
+                        replacement_method = st.selectbox("Choose Replacement Method From Below ", ['1. Previous Point', '2. 2*last-2nd_last', '3. Overall Mean', '4. 12_Point_Strategy', '5. Mean of Previous 2 points', '6. All Sequential', '7. All Parallel'])
+                        replacement_method=int(replacement_method[0])
                         if st.button("Compute"):
-                            if replacement_method>0:
-                                subprocess.run(["python", "psat_v3.py", str(constant_fk2d_value), str(multiplying_factor_value), str(Shear_velocity_value), str(option),str(corr), str(snr), str(lambda_value), str(k_value), str(replacement_method)])
-                                st.text("Computed! You can check your Results_v2.csv to see the computed values!!")
+                                if replacement_method>0:
+                                    start_time = datetime.now()
+                                    with st.spinner('Computing...'):
+                                        subprocess.run(["python", "psat_v3.py", str(constant_fk2d_value), str(multiplying_factor_value), str(Shear_velocity_value), str(option), str(corr), str(snr), str(lambda_value), str(k_value), str(replacement_method)])
+                                    end_time = datetime.now()
+                                    st.markdown("<h3>Computed! You can check your Results_v2.csv to see the computed values!!</h3>", unsafe_allow_html=True)
+                                    st.markdown("<h2>Run time</h2>", unsafe_allow_html=True)
+                                    st.write(f'Start time : {start_time.strftime("%c")}')
+                                    st.write(f'End time : {end_time.strftime("%c")}')
+                                    st.write(f'Duration : {end_time - start_time}')
     except:
         st.text("Something went wrong. Please refresh and try again!")
         exit(1)
